21c95120d1c42182cee5883b690aa5c7a0a48932
nonAlgebraic <sine@sinewave.me>
2019-03-31 15:53:55 -0400

    Implemented notification count badge

--- webview.js
+++ webview.js
@@ -1,4 +1,19 @@
 const path = require('path');
 
-// inject a single css file
-Franz.injectCSS(path.join(__dirname, 'style.css'));
+module.exports = Franz => {
+  const getNotifications = () => {
+    const notificationsGlobalItem = document.getElementById(
+      'notificationsGlobalItem'
+    );
+    const directMessages = notificationsGlobalItem
+      ? parseInt(notificationsGlobalItem.textContent, 10)
+      : 0;
+
+    Franz.setBadge(directMessages);
+  };
+
+  Franz.loop(getNotifications);
+
+  // inject a single css file
+  Franz.injectCSS(path.join(__dirname, 'style.css'));
+};
